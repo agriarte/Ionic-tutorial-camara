b88f64fe5e9c294f9c0370a0080ea64ef971683e
agriarte <agriarte@hotmail.com>
2021-06-08 10:29:26 +0200

    commit martes

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
+import { IPhoto } from './../services/interfaces';
 import { PhotoService } from './../services/photo.service';
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -8,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor(public photoService: PhotoService) { }
+  constructor(public photoService: PhotoService, public actionSheetController: ActionSheetController) {}
 
   // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
   async ngOnInit(){
@@ -17,6 +19,28 @@ export class Tab2Page {
 
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
+  }
+
+  public async showActionSheet(photo: IPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
   }
 
 }
